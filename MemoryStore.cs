7c83f085443ce24172316656ac8993d80267133e
Raazieyah <ST10465704@Rcconnect.edu.za>
2026-05-29 22:43:39 +0200

    Add files via upload
    Sentiment word detector and memory storing

---
+++ MemoryStore.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CyberSecurityChatbot
+{
+    public class MemoryStore // internal = public
+    {
+
+
+       
+
+        // stores basic information about the user
+        public string UserName { get; set; }
+        public string FavouriteTopic { get; set; }
+
+        
+        private Dictionary<string, string> _storage = new Dictionary<string, string>();
+
+        // saves information into our dictionary
+        public void Store(string key, string value)
+        {
+            _storage[key] = value;
+        }
+
+        // looks up information =  doesn't exist = "null" 
+        public string Recall(string key)
+        {
+            if (_storage.ContainsKey(key))
+            {
+                return _storage[key];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        // Creates a greeting based on whether we know their favorite topic
+        public string GetPersonalisedOpener()
+        {
+            if (string.IsNullOrEmpty(FavouriteTopic))
+            {
+                return "Hello " + UserName + "!";
+            }
+            else
+            {
+                return "As someone interested in " + FavouriteTopic + ", hello " + UserName + "!";
+            }
+        }
+
+        public class UserMemory // storing favourite topic
+        {
+            public string FavouriteTopic { get; set; }
+        }
+
+       
+        public void ProcessInput()
+        {
+           
+
+            var memory = new UserMemory();
+
+            string userInput = "I am interested in coding";
+
+            
+            if (userInput.Contains("I am interested in", StringComparison.OrdinalIgnoreCase))
+            {
+                string topic = userInput.Replace("I am interested in", "", StringComparison.OrdinalIgnoreCase).Trim();
+                memory.FavouriteTopic = topic;
+            }
+        }
+
+
+    }
+}
---
+++ SentimentDetector.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CyberSecurityChatbot
+{
+    
+      // inter class to public classs
+
+public enum Sentiment { Neutral, Worried, Curious, Frustrated, Happy }// our setiement words
+
+public class SentimentDetector
+{
+    //  list of trigger words
+    private Dictionary<Sentiment, List<string>> sentimentTriggers = new Dictionary<Sentiment, List<string>>
+    {// some trigger words from List
+        { Sentiment.Worried, new List<string> { "worried", "scared", "afraid", "anxious", "nervous", "unsafe" } },
+        { Sentiment.Curious, new List<string> { "curious", "wondering", "interested", "want to know", "how does" } },
+        { Sentiment.Frustrated, new List<string> { "frustrated", "annoyed", "confused", "don't understand" } },
+        { Sentiment.Happy, new List<string> { "great", "thanks", "helpful", "awesome", "love it" } }
+    };
+
+    // Detect(string input)
+    public Sentiment Detect(string input)
+    {
+        string lowerInput = input.ToLower();
+        foreach (var entry in sentimentTriggers)
+        {
+            if (entry.Value.Any(word => lowerInput.Contains(word)))
+            {
+                return entry.Key;
+            }
+        }
+        return Sentiment.Neutral;
+    }
+
+    //  return empathetic opening sentence
+    public string GetSentimentResponse(Sentiment s)
+    {
+        return s switch
+        {
+            Sentiment.Worried => "Do not worry, we are here to ease your concerns.",
+            Sentiment.Curious => "That is a good question.",
+            Sentiment.Frustrated => "We apologise for any confusion and frustrations caused.Lets get this problem sorted.",
+            Sentiment.Happy => "Im so glad we were able to help you.",
+            _ => "" // Neutral returns an empty string
+        };
+    }
+}
+
+    }
+
